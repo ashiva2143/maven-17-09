14adf67cee2f4b2794f3ed398c20120e53eefa46
ashiva2143 <shivaprasadgoudantharam@gmail.com>
2021-09-17 16:56:52 +0000

    code added by dev1

--- git-demo/src/main/java/com/shiva/app/App.java
+++ git-demo/src/main/java/com/shiva/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+	System.out.println( "changes by dev1");
         System.out.println( "Hello World!" );
 	System.out.println( "Changes made by dev2");
     }
